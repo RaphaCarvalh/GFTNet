28dfab3fa9e3846cda7681c75508eb6e91d2db1c
Rapha dev <devone.r8@gmail.com>
2022-02-14 03:01:54 -0300

    versão inicial
    2 componentes

---
+++ Projetos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Projetos.src.Entities;
+
+namespace projetos // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new Hero("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("ad", 1, "rcd");
+         
+
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(wizard.Attack());
+            
+        }
+    }
+}
